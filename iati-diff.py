3c955d3aa31b2962420c1d475eb79c9ef66b5265
kndm <kevindamato889@gmail.com>
2019-07-18 05:57:15 +0900

    Fix for duplicate fields, improved debug logging and output indentation

--- iati-diff.py
+++ iati-diff.py
@@ -1,37 +1,44 @@
-import xml.etree.ElementTree as ET
+
 import requests, shutil, os, sys
-from lxml import etree
+import logging
+from lxml import etree as ET
 from xmldiff import main, formatting
+
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
-
-elemList = []
-count = 0
 
 current_identifier = ''
 recording = False
 
 for elem in tree.iter():
-	print(elem.tag)
+	logging.debug("Elem tag is: {} ".format(elem.tag))
 	if elem.tag == 'iati-identifier':
-		if current_identifier != elem.text and current_identifier is not '':
-			if file:
-				file.close()
 
 		recording = True
-		current_identifier = elem.text
 		new_data = ET.Element('iati-activity')
 		new_data_identifier = ET.SubElement(new_data, 'iati-identifier')
+
+		logging.debug("Elem tag is iati-identifier")
+		if current_identifier != elem.text and current_identifier is not '':
+			logging.debug("Current identifier != elem.text and current_identifier is not empty: {} {}".format(elem.text, current_identifier))
+
+		current_identifier = elem.text
 		new_data_identifier.text = current_identifier
 
-	if recording == True:
+
+
+
+	if recording == True and elem.tag != 'iati-identifier':
 		current_child = ET.SubElement(new_data, elem.tag)
 		current_child.text = elem.text
-		new_data.append(current_child)
-		with open('./activities/' + current_identifier + '.xml', 'wb') as file:
-			print(new_data)
-			file.write(ET.tostring(new_data))
+		logging.debug("Old data is: {}".format(ET.tostring(new_data)))
+		#new_data.append(current_child)
+		logging.debug("New data is: {}".format(ET.tostring(new_data)))
+		if new_data:
+			with open('./activities/' + current_identifier + '.xml', 'wb') as file:
+				file.write(ET.tostring(new_data, pretty_print=True))
 
-	count = count+1
-	print(count)
+
+
