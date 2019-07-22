22fdfff9536accb0d2d5bd5b407a1ac5ea7c4bf9
kndm <kevindamato889@gmail.com>
2019-07-22 23:43:24 +0900

    Add file creation/deletion

--- iati-diff.py
+++ iati-diff.py
@@ -2,64 +2,94 @@
 import requests, shutil, os, sys
 import logging
 from lxml import etree as ET
-from xmldiff import main, formatting
+from xmldiff import main as diffile
+from xmldiff import formatting
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
 path_activities = "./activities/"
 path_differences = "./differences/"
 path_datastore = "./datastore/"
 
 
+def main():
+	recording = False
+	tree = ET.parse(sys.argv[1])
+	root = tree.getroot()
+	current_identifier = ''
 
-current_identifier = ''
-recording = False
+	for elem in tree.iter():
+		logging.debug("Elem tag is: {} ".format(elem.tag))
+		if elem.tag == 'iati-identifier':
 
-for elem in tree.iter():
-	logging.debug("Elem tag is: {} ".format(elem.tag))
-	if elem.tag == 'iati-identifier':
+			recording = True
+			new_data = ET.Element('iati-activity')
+			new_data_identifier = ET.SubElement(new_data, 'iati-identifier')
 
-		recording = True
-		new_data = ET.Element('iati-activity')
-		new_data_identifier = ET.SubElement(new_data, 'iati-identifier')
+			logging.debug("Elem tag is iati-identifier")
+			if current_identifier != elem.text and current_identifier is not '':
+				logging.debug("Current identifier != elem.text and current_identifier is not empty: {} {}".format(elem.text, current_identifier))
 
-		logging.debug("Elem tag is iati-identifier")
-		if current_identifier != elem.text and current_identifier is not '':
-			logging.debug("Current identifier != elem.text and current_identifier is not empty: {} {}".format(elem.text, current_identifier))
+			current_identifier = elem.text
+			new_data_identifier.text = current_identifier
 
-		current_identifier = elem.text
-		new_data_identifier.text = current_identifier
-
-	if recording == True and elem.tag != 'iati-identifier':
-		current_child = ET.SubElement(new_data, elem.tag)
-		current_child.text = elem.text
-		logging.debug("Old data is: {}".format(ET.tostring(new_data)))
-		#new_data.append(current_child)
-		logging.debug("New data is: {}".format(ET.tostring(new_data)))
-		if new_data:
-			with open('./activities/' + current_identifier + '.xml', 'wb') as file:
-				file.write(ET.tostring(new_data, pretty_print=True))
+		if recording == True and elem.tag != 'iati-identifier':
+			current_child = ET.SubElement(new_data, elem.tag)
+			current_child.text = elem.text
+			logging.debug("Old data is: {}".format(ET.tostring(new_data)))
+			#new_data.append(current_child)
+			logging.debug("New data is: {}".format(ET.tostring(new_data)))
+			if new_data:
+				with open(path_activities + current_identifier + '.xml', 'wb') as file:
+					file.write(ET.tostring(new_data, pretty_print=True))
 
 
-fileList = os.listdir("./activities/")
+	fileList = os.listdir(path_activities)
 
-elemList = [str(element).replace(".xml", "") for element in fileList]
-elemList.remove('.gitignore')
+	elemList = [str(element).replace(".xml", "") for element in fileList]
 
-logging.debug("Element List is: {}".format(elemList)) 
+	logging.debug("Element List is: {}".format(elemList)) 
 
-first = None
+	first = None
 
-formatter = formatting.DiffFormatter(pretty_print=True)
+	formatter = formatting.DiffFormatter(normalize=formatting.WS_BOTH, pretty_print=False)
 
-for filename in elemList:
-	datastore_xml_url = 'http://datastore.iatistandard.org/api/1/access/activity.xml?iati-identifier=' + str(filename)
-	response = requests.get(datastore_xml_url)
-	with open('./datastore/' + filename + '.xml', 'wb') as file:
-		file.write(response.content)
+	for filename in elemList:
+		datastore_xml_url = 'http://datastore.iatistandard.org/api/1/access/activity.xml?iati-identifier=' + str(filename)
+		response = requests.get(datastore_xml_url)
+		with open(path_datastore + filename + '.xml', 'wb') as file:
+			file.write(response.content)
 
-	with open('./differences/' + filename + '.txt', 'w') as diff_file:
-		for line in main.diff_files('./datastore/' + filename + '.xml' , './activities/' + filename + '.xml', formatter=formatter):
-			diff_file.write(line)
+		with open(path_differences + filename + '.txt', 'w') as diff_file:
+			for line in diffile.diff_files(path_datastore + filename + '.xml' , path_activities + filename + '.xml', formatter=formatter):
+				diff_file.write(line)
+
+
+
+def createFolders():
+	if not os.path.exists(path_activities):
+		os.makedirs(path_activities)
+
+	if not os.path.exists(path_datastore):
+		os.makedirs(path_datastore)
+
+	if not os.path.exists(path_differences):
+		os.makedirs(path_differences)
+
+
+def deleteFolders():
+
+	if os.path.exists(path_activities):
+		shutil.rmtree(path_activities)
+
+	if os.path.exists(path_datastore):
+		shutil.rmtree(path_datastore)
+
+def remove_newlines(fname):
+    flist = open(fname).readlines()
+    return [s.rstrip('\n') for s in flist]
+
+
+if __name__ == '__main__':
+	createFolders()
+	main()
